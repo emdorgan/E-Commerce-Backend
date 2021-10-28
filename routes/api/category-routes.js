c8da25c3455b645ca4da43ca9ae60021780ffb3b
Emily Dorgan <wmdorgan@gmail.com>
2021-10-27 21:07:16 -0700

    setup the get route for the /api/categories after testing that the sequelize model is working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,26 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // create an instance of the model
+    const categoryData = await Category.findAll();
+    // parse out the relevant data
+    const categories = categoryData.map((category) => category.get({ plain: true}));
+    // send it to the user
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
