d776c7fc2ff30aded7f4a3d1737da995b33ca583
Emily Dorgan <wmdorgan@gmail.com>
2021-10-28 19:09:52 -0700

    added most of the get all product GET route, added the ability for the get all Categories to return the products in each category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,31 +3,31 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// GET route that returns all categories and their products
 router.get('/', async (req, res) => {
   try {
-    // create an instance of the model
-    const categoryData = await Category.findAll();
-    // parse out the relevant data from the findAll()
+    // create an instance of the model, parse out relevant data with .get() presents it to the user
+    const categoryData = await Category.findAll({
+      include: { model: Product },
+    });
     const categories = categoryData.map((category) => category.get({ plain: true}));
-    // send it to the user
     res.status(200).json(categories);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// GET route that returns the category of the stated ID
 router.get('/:id', async (req, res) => {
   try {
     // database query based on primary key in category table, includes the product and all it's associated info
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{model: Product}],
     });
-    // givse the user an error for invalid IDs.
     if(!categoryData){
       res.status(404).json({message: 'Error: not a valid ID'});
       return;
     }
-    //Provides that category (and it's associated products) to the user
     res.status(200).json(categoryData);
 
   } catch (err) {
@@ -35,6 +35,7 @@ router.get('/:id', async (req, res) => {
 }
 });
 
+//POST route to add a category
 router.post('/', async (req, res) => {
   try {
     // adds a category to the database, espects a JSON object with a single key of category_names
@@ -46,6 +47,7 @@ router.post('/', async (req, res) => {
 }
 });
 
+//PUT route to update a category of the given id
 router.put('/:id', async (req, res) => {
   try {
     const categoryData = await Category.update({
@@ -54,7 +56,6 @@ router.put('/:id', async (req, res) => {
       {
         where: {id: req.params.id}
       })
-    console.log(`category name has been updated to ${req.body.category_name}`);
     res.status(200).json({message: `category name has been updated to ${req.body.category_name}`});
   } catch (err) {
     res.status(500).json(err);
@@ -68,12 +69,11 @@ router.delete('/:id', async (req, res) => {
         where: {id: req.params.id}
       });
     
-    if(!categoryData){
+    if(!categoryData) {
       res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
-    console.log(categoryData);
-    res.status(200).json(categoryData);
+    res.status(200).json({message: `Category deleted`});
   } catch (err) {
     res.status(500).json(err);
   }
