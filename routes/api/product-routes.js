d12f09fe9575e597fa6ecbf991e2b2ceebbe1013
Emily Dorgan <wmdorgan@gmail.com>
2021-10-28 20:09:48 -0700

    set up all the product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -3,13 +3,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
-// get all products
+// get all products with their associated category and tag data
 router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{ model: Tag, through: ProductTag, as: 'tags'}]
+      include: [
+        { model: Tag, through: ProductTag, as: 'tags' }, 
+        { model: Category }
+      ],
     });
     const products = productData.map((product) => product.get({plain: true}));
     res.status(200).json(products);
@@ -19,11 +22,19 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-  try{
-
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Tag, through: ProductTag, as: 'tags'},
+        { model: Category }
+      ],
+    });
+    if(!productData){
+      res.status(404).json({message: 'Error: not a valid ID'});
+      return;
+    }
+    res.status(200).json(productData)
   } catch (err) {
     res.stats(500).json(err);
   }
@@ -103,10 +114,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try{
-
+      const productData = await Product.destroy(
+        {
+          where: {id: req.params.id}
+        });
+      if(!productData) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+      res.status(200).json({message:`Product deleted`});
   } catch (err) {
     res.stats(500).json(err);
   }
